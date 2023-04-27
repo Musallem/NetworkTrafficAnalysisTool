63de2436dca7b85b05ec656eb5e14fe0b0b87c21
Musallem <105140931+Musallem@users.noreply.github.com>
2023-04-27 11:02:18 +0545

    Delete test_pyshark.py

--- NetworkTrafficAnalysisTool/test_pyshark.py
+++
@@ -1,7 +0,0 @@
-import pyshark
-
-capture = pyshark.LiveCapture(interface='WiFi')
-capture.sniff(timeout=10)
-for packet in capture.sniff_continuously(packet_count=5):
-    print('Just arrived:', packet)
-
