4bca26a25173262f82d822ed8f18941244f3b6b5
Musallem <105140931+Musallem@users.noreply.github.com>
2023-04-27 17:11:23 +0300

    Update covert_channel_detection.py

--- NetworkTrafficAnalysisTool/covert_channel_detection.py
+++ NetworkTrafficAnalysisTool/covert_channel_detection.py
@@ -4,6 +4,10 @@ from sklearn.impute import KNNImputer
 import pandas as pd
 import os
 from tabulate import tabulate
+import sys
+from os.path import dirname, realpath, join
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QTableWidget, QTableWidgetItem
+from PyQt5.uic import loadUiType
 
 # Write model path
 TCP_model = joblib.load('rfmodel_trained_seq_chk_ack.joblib')
@@ -67,105 +71,149 @@ def ip_features_extraction(pkt):
 
 imputer = KNNImputer()
 
-file_path = input('Enter pcap file path: ').replace('\\', '/').replace("'", '').replace('"', '')
-
-if not os.path.isfile(file_path):
-    print('Error: File does not exist')
-    exit()
-
-input_pcap = pyshark.FileCapture(file_path)
-packet_details = []
-packet_num = 0
-
-for pkt in input_pcap:
-    fields = {
-        'Packet Number': packet_num,
-        'Time': pkt.sniff_time.strftime('%Y-%m-%d %H:%M:%S.%f'),
-        'Source IP': pkt.ip.src if 'IP' in pkt else '',
-        'Destination IP': pkt.ip.dst if 'IP' in pkt else '',
-        'Source Port': pkt[pkt.transport_layer].srcport if pkt.transport_layer else '',
-        'Destination Port': pkt[pkt.transport_layer].dstport if pkt.transport_layer else '',
-        'Protocol': pkt.transport_layer if pkt.transport_layer else pkt.highest_layer
-    }
-    packet_details.append(fields)
-    packet_num += 1
-
-if packet_details:
-    print('Packet Details:')
-    packet_table = tabulate(packet_details, headers='keys', tablefmt='psql', showindex=True)
-    print(packet_table)
-else:
-    print('No packets found.')
-
-input_pcap.reset()
 
 
-tcp_X = pd.DataFrame(columns=['ip_id', 'tcp_seq', 'tcp_seq_raw', 'tcp_ack', 'tcp_ack_raw', 'tcp_len', 'tcp_hdr_len', 'tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum'])
-ip_X = pd.DataFrame(columns=['ip_id', 'ip_ttl', 'ip_checksum', 'ip_flags'])
+scriptDir = dirname(realpath(__file__))
+From_Main, _ = loadUiType(join(dirname(__file__), "DetectCovertChannel.ui"))
 
-for pkt in input_pcap:
-    if 'TCP' in pkt:
-        tcp_packet_features = tcp_features_extraction(pkt)
-        if tcp_packet_features is not None:
-            tcp_X = pd.concat([tcp_X,tcp_packet_features], ignore_index=True)
+class MainWindow(QWidget, From_Main):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        QWidget.__init__(self)
+        self.setupUi(self)
+
+        self.ButtonOpen.clicked.connect(self.OpenFile)
+        self.BtnDescribe.clicked.connect(self.dataHead)
+
+    def OpenFile(self):
+        try:
+            path = QFileDialog.getOpenFileName(self, 'Open PCAP', os.getenv('HOME'), 'PCAP(*.pcap)')[0]
+
+
+            if not os.path.isfile(path):
+                print('Error: File does not exist')
+                exit()
+
+            input_pcap = pyshark.FileCapture(path)
+            packet_details = []
+            packet_num = 0
+
+            for pkt in input_pcap:
+                fields = {
+                    'Packet Number': packet_num,
+                    'Time': pkt.sniff_time.strftime('%Y-%m-%d %H:%M:%S.%f'),
+                    'Source IP': pkt.ip.src if 'IP' in pkt else '',
+                    'Destination IP': pkt.ip.dst if 'IP' in pkt else '',
+                    'Source Port': pkt[pkt.transport_layer].srcport if pkt.transport_layer else '',
+                    'Destination Port': pkt[pkt.transport_layer].dstport if pkt.transport_layer else '',
+                    'Protocol': pkt.transport_layer if pkt.transport_layer else pkt.highest_layer
+                }
+                packet_details.append(fields)
+                packet_num += 1
+
+            if packet_details:
+
+                packet_table = tabulate(packet_details, headers='keys', tablefmt='psql', showindex=True)
+                # print(packet_table)
+            else:
+                print('No packets found.')
+
+            input_pcap.reset()
+
+            tcp_X = pd.DataFrame(
+                columns=['ip_id', 'tcp_seq', 'tcp_seq_raw', 'tcp_ack', 'tcp_ack_raw', 'tcp_len', 'tcp_hdr_len',
+                         'tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum'])
+            ip_X = pd.DataFrame(columns=['ip_id', 'ip_ttl', 'ip_checksum', 'ip_flags'])
+
+            for pkt in input_pcap:
+                if 'TCP' in pkt:
+                    tcp_packet_features = tcp_features_extraction(pkt)
+                    if tcp_packet_features is not None:
+                        tcp_X = pd.concat([tcp_X, tcp_packet_features], ignore_index=True)
+                    else:
+                        pass
+
+                elif 'IP' in pkt:
+                    ip_packet_features = ip_features_extraction(pkt)
+                    if ip_packet_features is not None:
+                        ip_X = pd.concat([ip_X, ip_packet_features], ignore_index=True)
+                    else:
+                        pass
+                else:
+                    pass
+
+            if not tcp_X.empty:
+                imputer = KNNImputer()
+                tcp_X[['ip_id', 'tcp_seq', 'tcp_seq_raw',
+                       'tcp_ack', 'tcp_ack_raw', 'tcp_len', 'tcp_hdr_len',
+                       'tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum']] = imputer.fit_transform(
+                    tcp_X[['ip_id', 'tcp_seq',
+                           'tcp_seq_raw', 'tcp_ack',
+                           'tcp_ack_raw', 'tcp_len',
+                           'tcp_hdr_len', 'tcp_flags_syn',
+                           'tcp_flags_ack', 'tcp_checksum']])
+
+            class_label = {0: "covert", 1: "normal"}
+
+            if not ip_X.empty:
+                imputer = KNNImputer()
+                ip_X[['ip_id', 'ip_ttl', 'ip_checksum', 'ip_flags']] = imputer.fit_transform(
+                    ip_X[['ip_id', 'ip_ttl', 'ip_checksum', 'ip_flags']])
+
+            if not tcp_X.empty:
+                tcp_predictions = TCP_model.predict(tcp_X)
+                tcp_prediction_probs = TCP_model.predict_proba(tcp_X)
+                tcp_prediction_probs_max = tcp_prediction_probs.max(axis=1)
+
+                tcp_X['prediction'] = tcp_predictions
+                tcp_X['prediction'] = tcp_X['prediction'].map(class_label)
+                tcp_X['confidence_score'] = tcp_prediction_probs_max
+
+                tcp_table = tabulate(tcp_X, headers='keys')
+                # print(tcp_table)
+                self.all_data = tcp_X
+            else:
+                print('No TCP packets found.')
+                tcp_predictions = []
+
+            if not ip_X.empty:
+                ip_predictions = IP_model.predict(ip_X)
+                ip_prediction_probs = IP_model.predict_proba(ip_X)
+                ip_prediction_probs_max = ip_prediction_probs.max(axis=1)
+
+                ip_X['prediction'] = ip_predictions
+                ip_X['prediction'] = ip_X['prediction'].map(class_label)
+                ip_X['confidence_score'] = ip_prediction_probs_max
+                # format ip detection
+                ip_table = tabulate(ip_X, headers='keys')
+                # print(ip_table)
+                self.all_data = ip_X
+            else:
+                print('No IP packets found.')
+                ip_predictions = []
+
+        except:
+            print(path)
+
+    def dataHead(self):
+        numColomn = self.spinBox.value()
+        if numColomn == 0:
+            NumRows = len(self.all_data.index)
         else:
-            pass
+            NumRows = numColomn
+        self.tableWidget.setColumnCount(len(self.all_data.columns))
+        self.tableWidget.setRowCount(NumRows)
+        self.tableWidget.setHorizontalHeaderLabels(self.all_data.columns)
 
-    elif 'IP' in pkt:
-        ip_packet_features = ip_features_extraction(pkt)
-        if ip_packet_features is not None:
-            ip_X = pd.concat([ip_X,ip_packet_features], ignore_index=True)
-        else:
-            pass
-    else:
-        pass
+        for i in range(NumRows):
+            for j in range(len(self.all_data.columns)):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(self.all_data.iat[i, j])))
 
-
-if not tcp_X.empty:
-    imputer = KNNImputer()
-    tcp_X[['ip_id', 'tcp_seq', 'tcp_seq_raw',
-           'tcp_ack', 'tcp_ack_raw', 'tcp_len', 'tcp_hdr_len',
-           'tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum']] = imputer.fit_transform(tcp_X[['ip_id', 'tcp_seq',
-                                                                                             'tcp_seq_raw', 'tcp_ack',
-                                                                                             'tcp_ack_raw', 'tcp_len',
-                                                                                             'tcp_hdr_len', 'tcp_flags_syn',
-                                                                                             'tcp_flags_ack', 'tcp_checksum']])
-
-class_label = {0: "covert", 1: "normal"}
-
-if not ip_X.empty:
-    imputer = KNNImputer()
-    ip_X[['ip_id', 'ip_ttl', 'ip_checksum', 'ip_flags']] = imputer.fit_transform(ip_X[['ip_id', 'ip_ttl', 'ip_checksum', 'ip_flags']])
+        self.tableWidget.resizeColumnsToContents()
+        self.tableWidget.resizeRowsToContents()
 
 
-if not tcp_X.empty:
-    tcp_predictions = TCP_model.predict(tcp_X)
-    tcp_prediction_probs = TCP_model.predict_proba(tcp_X)
-    tcp_prediction_probs_max = tcp_prediction_probs.max(axis=1)
-
-    tcp_X['prediction'] = tcp_predictions
-    tcp_X['prediction'] = tcp_X['prediction'].map(class_label)
-    tcp_X['confidence_score'] = tcp_prediction_probs_max
-
-    tcp_table = tabulate(tcp_X, headers='keys', tablefmt='psql')
-    print('TCP Packets:')
-    print(tcp_table)
-else:
-    print('No TCP packets found.')
-    tcp_predictions = []
-
-if not ip_X.empty:
-    ip_predictions = IP_model.predict(ip_X)
-    ip_prediction_probs = IP_model.predict_proba(ip_X)
-    ip_prediction_probs_max = ip_prediction_probs.max(axis=1)
-
-    ip_X['prediction'] = ip_predictions
-    ip_X['prediction'] = ip_X['prediction'].map(class_label)
-    ip_X['confidence_score'] = ip_prediction_probs_max
-    #format ip detection
-    ip_table = tabulate(ip_X, headers='keys', tablefmt='psql')
-    print('IP Packets:')
-    print(ip_table)
-else:
-    print('No IP packets found.')
-    ip_predictions = []
+app = QApplication(sys.argv)
+sheet = MainWindow()
+sheet.show()
+sys.exit(app.exec_())
