44b67e6a3a5dd01af76e42a92ba7a0b10bbae5f2
Musallem <105140931+Musallem@users.noreply.github.com>
2023-06-17 01:48:47 +0545

    Update covert_channel_detection_gui.py

--- NetworkTrafficAnalysisTool/covert_channel_detection_gui.py
+++ NetworkTrafficAnalysisTool/covert_channel_detection_gui.py
@@ -2,9 +2,8 @@ import joblib
 import pyshark
 from sklearn.impute import KNNImputer
 import pandas as pd
+from scapy.all import IP, TCP, Ether
 import os
-from scapy.all import *
-
 from tabulate import tabulate
 import sys
 from os.path import dirname, realpath, join
@@ -13,25 +12,29 @@ from PyQt5.uic import loadUiType
 
 # Write model path
 TCP_model = joblib.load('rfmodel_isn_p15.joblib')
-TCP_model.feature_names = ['tcp_seq', 'tcp_ack',
-                           'tcp_flags_syn', 'tcp_checksum']
+TCP_model.feature_names = ['tcp_seq', 'tcp_ack','tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum']
 
 # Write model path
 IP_model = joblib.load('rfmodel_ip_id_p15.joblib')
-IP_model.feature_names = ['ip_len', 'ip_id', 'ip_ttl', 'ip_flags_df']
+IP_model.feature_names = ['ip_id', 'ip_ttl', 'ip_len', 'ip_flags_df']
 
 
 def tcp_features_extraction(pkt):
-    if pkt.haslayer(TCP) and pkt.haslayer(IP):
-        tcp_seq = pkt[TCP].seq
-        tcp_ack = pkt[TCP].ack
-        tcp_flags_syn = pkt[TCP].flags.S
-        tcp_checksum = pkt[TCP].chksum
+    if 'TCP' in pkt and 'IP' in pkt:
+        tcp_seq = pkt.tcp.seq
+        tcp_ack = pkt.tcp.ack
+        tcp_flags_syn = pkt.tcp.flags_syn
+        tcp_flags_ack = pkt.tcp.flags_ack
+        tcp_checksum = int(pkt.tcp.checksum, 16)
+
+
+
 
         df = pd.DataFrame({
             'tcp_seq': [tcp_seq],
             'tcp_ack': [tcp_ack],
             'tcp_flags_syn': [tcp_flags_syn],
+            'tcp_flags_ack': [tcp_flags_ack],
             'tcp_checksum': [tcp_checksum]
         })
         return df
@@ -39,17 +42,18 @@ def tcp_features_extraction(pkt):
         return None
 
 def ip_features_extraction(pkt):
-    if pkt.transport_layer:
+    if 'TCP' in pkt:
         ip_id = int(pkt.ip.id, 16)
         ip_ttl = pkt.ip.ttl
-        ip_checksum = int(pkt.ip.checksum, 16)
-        ip_flags = int(pkt.ip.flags, 16)
+        ip_len = pkt.ip.len
+        ip_flags_df = pkt.ip.flags.df
+
 
         df = pd.DataFrame({
             'ip_id': [ip_id],
             'ip_ttl': [ip_ttl],
-            'ip_checksum': [ip_checksum],
-            'ip_flags': [ip_flags]
+            'ip_len': [ip_len],
+            'ip_flags_df': [ip_flags_df]
         })
         return df
     else:
@@ -62,13 +66,9 @@ imputer = KNNImputer()
 scriptDir = dirname(realpath(__file__))
 From_Main, _ = loadUiType(join(dirname(__file__), "DetectCovertChannel.ui"))
 
-all_data_tcp = pd.DataFrame()
-
-all_data_ip = pd.DataFrame()
-
-class CcdWindow(QWidget, From_Main):
+class MainWindow(QWidget, From_Main):
     def __init__(self):
-        super(CcdWindow, self).__init__()
+        super(MainWindow, self).__init__()
         QWidget.__init__(self)
         self.setupUi(self)
 
@@ -85,10 +85,9 @@ class CcdWindow(QWidget, From_Main):
                 print('Error: File does not exist')
                 exit()
 
-            input_pcap = rdpcap(path)
+            input_pcap = pyshark.FileCapture(path)
             packet_details = []
             packet_num = 0
-
 
             for pkt in input_pcap:
                 fields = {
@@ -106,22 +105,21 @@ class CcdWindow(QWidget, From_Main):
             if packet_details:
 
                 packet_table = tabulate(packet_details, headers='keys', tablefmt='psql', showindex=True)
-                print(packet_table)
+                # print(packet_table)
             else:
                 print('No packets found.')
 
             input_pcap.reset()
 
-            tcp_X = pd.DataFrame(
-                columns=['tcp_seq', 'tcp_ack',
-                         'tcp_flags_syn', 'tcp_checksum'])
-            ip_X = pd.DataFrame(columns=['ip_len', 'ip_id', 'ip_ttl', 'ip_flags_df'])
+            tcp_X = pd.DataFrame(columns=['tcp_seq', 'tcp_ack','tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum'])
+            ip_X = pd.DataFrame(columns=['ip_id', 'ip_ttl', 'ip_len', 'ip_flags_df'])
 
             for pkt in input_pcap:
                 if 'TCP' in pkt:
                     tcp_packet_features = tcp_features_extraction(pkt)
                     if tcp_packet_features is not None:
                         tcp_X = pd.concat([tcp_X, tcp_packet_features], ignore_index=True)
+                        print(tcp_X)
                     else:
                         pass
 
@@ -136,21 +134,19 @@ class CcdWindow(QWidget, From_Main):
 
             if not tcp_X.empty:
                 imputer = KNNImputer()
-                tcp_X[['tcp_seq', 'tcp_ack',
-                       'tcp_flags_syn', 'tcp_checksum']] = imputer.fit_transform(
-                    tcp_X[['tcp_seq', 'tcp_ack',
-                           'tcp_flags_syn', 'tcp_checksum']])
+                tcp_X[['tcp_seq', 'tcp_ack','tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum']] = imputer.fit_transform(tcp_X[['tcp_seq', 'tcp_ack','tcp_flags_syn', 'tcp_flags_ack', 'tcp_checksum']])
 
             class_label = {0: "covert", 1: "normal"}
 
             if not ip_X.empty:
                 imputer = KNNImputer()
-                ip_X[['ip_len', 'ip_id', 'ip_ttl', 'ip_flags_df']] = imputer.fit_transform(
-                    ip_X[['ip_len', 'ip_id', 'ip_ttl', 'ip_flags_df']])
+                ip_X[['ip_id', 'ip_ttl', 'ip_len', 'ip_flags_df']] = imputer.fit_transform(ip_X[['ip_id', 'ip_ttl', 'ip_len', 'ip_flags_df']])
 
             if not tcp_X.empty:
                 tcp_predictions = TCP_model.predict(tcp_X)
+                print(tcp_predictions)
                 tcp_prediction_probs = TCP_model.predict_proba(tcp_X)
+                print(tcp_prediction_probs)
                 tcp_prediction_probs_max = tcp_prediction_probs.max(axis=1)
 
                 tcp_X['prediction'] = tcp_predictions
@@ -159,8 +155,7 @@ class CcdWindow(QWidget, From_Main):
 
                 tcp_table = tabulate(tcp_X, headers='keys')
                 print(tcp_table)
-
-                all_data_tcp = tcp_X
+                self.all_data_tcp = tcp_X
             else:
                 print('No TCP packets found.')
                 tcp_predictions = []
@@ -176,8 +171,7 @@ class CcdWindow(QWidget, From_Main):
                 # format ip detection
                 ip_table = tabulate(ip_X, headers='keys')
                 print(ip_table)
-
-                all_data_ip = ip_X
+                self.all_data_ip = ip_X
             else:
                 print('No IP packets found.')
                 ip_predictions = []
@@ -186,43 +180,41 @@ class CcdWindow(QWidget, From_Main):
             print(path)
 
     def dataHead_tcp(self):
-
         numColomn = self.spinBox.value()
         if numColomn == 0:
-            NumRows = len(all_data_tcp.index)
+            NumRows = len(self.all_data_tcp.index)
         else:
             NumRows = numColomn
-        self.tableWidget.setColumnCount(len(all_data_tcp.columns))
+        self.tableWidget.setColumnCount(len(self.all_data_tcp.columns))
         self.tableWidget.setRowCount(NumRows)
-        self.tableWidget.setHorizontalHeaderLabels(all_data_tcp.columns)
+        self.tableWidget.setHorizontalHeaderLabels(self.all_data_tcp.columns)
 
         for i in range(NumRows):
-            for j in range(len(all_data_tcp.columns)):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(all_data_tcp.iat[i, j])))
+            for j in range(len(self.all_data_tcp.columns)):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(self.all_data_tcp.iat[i, j])))
 
         self.tableWidget.resizeColumnsToContents()
         self.tableWidget.resizeRowsToContents()
 
     def dataHead_ip(self):
-
         numColomn = self.spinBox_2.value()
         if numColomn == 0:
-            NumRows = len(all_data_ip.index)
+            NumRows = len(self.all_data_ip.index)
         else:
             NumRows = numColomn
-        self.tableWidget_2.setColumnCount(len(all_data_ip.columns))
+        self.tableWidget_2.setColumnCount(len(self.all_data_ip.columns))
         self.tableWidget_2.setRowCount(NumRows)
-        self.tableWidget_2.setHorizontalHeaderLabels(all_data_ip.columns)
+        self.tableWidget_2.setHorizontalHeaderLabels(self.all_data_ip.columns)
 
         for i in range(NumRows):
-            for j in range(len(all_data_ip.columns)):
-                self.tableWidget_2.setItem(i, j, QTableWidgetItem(str(all_data_ip.iat[i, j])))
+            for j in range(len(self.all_data_ip.columns)):
+                self.tableWidget_2.setItem(i, j, QTableWidgetItem(str(self.all_data_ip.iat[i, j])))
 
         self.tableWidget_2.resizeColumnsToContents()
         self.tableWidget_2.resizeRowsToContents()
 
 
-# app = QApplication(sys.argv)
-# sheet = MainWindow()
-# sheet.show()
-# sys.exit(app.exec_())
+app = QApplication(sys.argv)
+sheet = MainWindow()
+sheet.show()
+sys.exit(app.exec_())
